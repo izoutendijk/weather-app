b70bfae74e6250f1bcbf269b0690309d88e44759
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-04-30 15:31:48 +0200

    Replaced all http to https

--- src/script.js
+++ src/script.js
@@ -73,7 +73,7 @@ function handleWeather(response) {
   let icon = document.querySelector("#icon");
   icon.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
 
